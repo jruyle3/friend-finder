6e9789f47fbca831ee2a3ab4ad14ca9d0a8b33a1
James Ruyle <jruyle3@gmail.com>
2019-01-19 11:34:09 -0800

    hw

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friendMatch = [
+    {
+      name: "Daenarys Targaryen",
+      photo: "https://i.pinimg.com/originals/46/03/86/46038600348d54aa5eb35d8792fcf575.jpg ",
+      scores: ["2", "1", "2", "1", "1", "2", "3", "3", "2", "1"]
+    }, {
+      name: "Jon Snow",
+      photo: "https://s3.r29static.com//bin/entry/4f0/720x864,85/1801540/image.jpg ",
+      scores: ["3", "1", "1", "3", "2", "2", "1", "1", "2", "2",]
+    }, {
+      name: "Tyrion Lannister",
+      photo: "http://watchersonthewall.com/wp-content/uploads/2017/08/Tyrion-Lannister-Eastwatch.jpg",
+      scores: ["1", "2", "3", "2", "1", "2", "3", "2", "1", "2"]
+    }, {
+      name: "Night King",
+      photo: "https://cdn.vox-cdn.com/thumbor/gm4o5aqfVGJ11jM6JRugpXfOFgE=/0x0:3733x2100/1200x800/filters:focal(1042x347:1638x943)/cdn.vox-cdn.com/uploads/chorus_image/image/56282285/1508_promo_stills_12001692731.0.jpg",
+      scores: ["3", "2", "1", "2", "1", "1", "1", "2", "2", "3",]
+    },
+  
+  ];
+  
+  
+  
+  module.exports = friendMatch;
